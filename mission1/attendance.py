19cb322985640dc700cd5f950e15383c133326b5
jungsh83 <jungsh83@gmail.com>
2025-08-01 09:49:58 +0900

    [refact] mission1 global 변수를 class 변수로 이동시켰음

--- mission1/attendance.py
+++ mission1/attendance.py
@@ -1,26 +1,27 @@
 from pathlib import Path
 
-id1 = {}
-id_cnt = 0
-
-# dat[사용자ID][요일]
-dat = [[0] * 100 for _ in range(100)]
-points = [0] * 100
-grade = [0] * 100
-names = [''] * 100
-wed = [0] * 100
-weeken = [0] * 100
-
 class Attendance:
+
+    def __init__(self):
+        self.id1 = {}
+        self.id_cnt = 0
+
+        # dat[사용자ID][요일]
+        self.dat = [[0] * 100 for _ in range(100)]
+        self.points = [0] * 100
+        self.grade = [0] * 100
+        self.names = [''] * 100
+        self.wed = [0] * 100
+        self.weeken = [0] * 100
+
     def input2(self, w, wk):
-        global id_cnt
 
-        if w not in id1:
-            id_cnt += 1
-            id1[w] = id_cnt
-            names[id_cnt] = w
+        if w not in self.id1:
+            self.id_cnt += 1
+            self.id1[w] = self.id_cnt
+            self.names[self.id_cnt] = w
 
-        id2 = id1[w]
+        id2 = self.id1[w]
 
         add_point = 0
         index = 0
@@ -34,7 +35,7 @@ class Attendance:
         elif wk == "wednesday":
             index = 2
             add_point += 3
-            wed[id2] += 1
+            self.wed[id2] += 1
         elif wk == "thursday":
             index = 3
             add_point += 1
@@ -44,14 +45,14 @@ class Attendance:
         elif wk == "saturday":
             index = 5
             add_point += 2
-            weeken[id2] += 1
+            self.weeken[id2] += 1
         elif wk == "sunday":
             index = 6
             add_point += 2
-            weeken[id2] += 1
+            self.weeken[id2] += 1
 
-        dat[id2][index] += 1
-        points[id2] += add_point
+        self.dat[id2][index] += 1
+        self.points[id2] += add_point
 
     def input_file(self):
         try:
@@ -64,36 +65,37 @@ class Attendance:
                     parts = line.strip().split()
                     if len(parts) == 2:
                         self.input2(parts[0], parts[1])
-
-            for i in range(1, id_cnt + 1):
-                if dat[i][3] > 9:
-                    points[i] += 10
-                if dat[i][5] + dat[i][6] > 9:
-                    points[i] += 10
-
-                if points[i] >= 50:
-                    grade[i] = 1
-                elif points[i] >= 30:
-                    grade[i] = 2
-                else:
-                    grade[i] = 0
-
-                print(f"NAME : {names[i]}, POINT : {points[i]}, GRADE : ", end="")
-                if grade[i] == 1:
-                    print("GOLD")
-                elif grade[i] == 2:
-                    print("SILVER")
-                else:
-                    print("NORMAL")
-
-            print("\nRemoved player")
-            print("==============")
-            for i in range(1, id_cnt + 1):
-                if grade[i] not in (1, 2) and wed[i] == 0 and weeken[i] == 0:
-                    print(names[i])
-
         except FileNotFoundError:
             print("파일을 찾을 수 없습니다.")
+
+        for i in range(1, self.id_cnt + 1):
+            if self.dat[i][3] > 9:
+                self.points[i] += 10
+            if self.dat[i][5] + self.dat[i][6] > 9:
+                self.points[i] += 10
+
+            if self.points[i] >= 50:
+                self.grade[i] = 1
+            elif self.points[i] >= 30:
+                self.grade[i] = 2
+            else:
+                self.grade[i] = 0
+
+            print(f"NAME : {self.names[i]}, POINT : {self.points[i]}, GRADE : ", end="")
+            if self.grade[i] == 1:
+                print("GOLD")
+            elif self.grade[i] == 2:
+                print("SILVER")
+            else:
+                print("NORMAL")
+
+        print("\nRemoved player")
+        print("==============")
+        for i in range(1, self.id_cnt + 1):
+            if self.grade[i] not in (1, 2) and self.wed[i] == 0 and self.weeken[i] == 0:
+                print(self.names[i])
+
+
 
 if __name__ == "__main__":
     attendance = Attendance()
